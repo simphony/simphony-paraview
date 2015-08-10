53c66d5a1dfe4008e58581a5d9f3b881cd87225c
itziakos <ioannist@enthought.com>
2015-08-10 18:56:31 +0100

    add iterator for iterating over cell links of a UnstructuredGrid

--- simphony_paraview/core/iterators.py
+++ simphony_paraview/core/iterators.py
@@ -1,4 +1,5 @@
 from paraview import vtk
+from paraview.numpy_support import vtk_to_numpy
 
 
 def iter_cells(cell_array):
@@ -7,3 +8,19 @@ def iter_cells(cell_array):
     ids = vtk.vtkIdList()
     while cell_array.GetNextCell(ids) == 1:
         yield [ids.GetId(i) for i in range(ids.GetNumberOfIds())]
+
+
+def iter_grid_cells(grid, cell_types=None):
+    """ Iterate over cells in a vtkUnstructuredGrid filtering on CellTypes. """
+    if cell_types is None:
+        for index in range(grid.GetNumberOfCells()):
+            cell = grid.GetCell(index)
+            ids = cell.GetPointIds()
+            yield [ids.GetId(i) for i in range(ids.GetNumberOfIds())]
+    else:
+        cell_types_array = vtk_to_numpy(grid.GetCellTypesArray())
+        for index, cell_type in enumerate(cell_types_array):
+            if cell_type in cell_types:
+                cell = grid.GetCell(index)
+                ids = cell.GetPointIds()
+                yield [ids.GetId(i) for i in range(ids.GetNumberOfIds())]
